19e64eefaf684879e92830f9933570bfadfe6370
Juraj Letko <jletko@gmail.com>
2020-03-15 16:32:37 +0100

    Start tensor board chrome non-blocking

--- Training/Scripts/board.bat
+++ Training/Scripts/board.bat
@@ -1,4 +1,4 @@
-echo off
+@echo off
 
 TASKLIST | FINDSTR "tensorboard" || start /min tensorboard --logdir=summaries --port 6006"
-"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --new-window "http://localhost:6006"
+start "Chrome" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --new-window "http://localhost:6006"
