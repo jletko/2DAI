3dccc446990058a6f6c1842a2dd39a4509215e21
Juraj Letko <jletko@gmail.com>
2020-03-15 12:35:21 +0100

    trainer_config and training scripts moved to unity Assets\Training folder to be versioned.

---
+++ Assets/Training/board.bat
@@ -0,0 +1,3 @@
+@echo off
+TASKLIST | FINDSTR "tensorboard" || start /min tensorboard --logdir=summaries --port 6006"
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --new-window "http://localhost:6006"
---
+++ Assets/Training/mlenv.bat
@@ -0,0 +1,8 @@
+echo off
+SET PATH=%PATH%;D:\Documents\_git\2DAI\Assets\Training
+d:
+cd D:\Documents\_git\ml-agents
+cmd /k D:\Programs\Python\Virtual\MLAgents\Scripts\activate
+
+
+
