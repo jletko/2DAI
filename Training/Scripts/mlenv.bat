f4f56c27088d350814f5810b925a0d3a3d76b8d1
Juraj Letko <jletko@gmail.com>
2020-03-15 15:59:25 +0100

    Training scripts moved outside Assets directory.

--- Training/Scripts/mlenv.bat
+++ Training/Scripts/mlenv.bat
@@ -1,6 +1,6 @@
 @echo off
 
-SET PATH=%PATH%;D:\Documents\_git\2DAI\Assets\Training\Scripts
+SET PATH=%PATH%;D:\Documents\_git\2DAI\Training\Scripts
 d:
 cd D:\Documents\_git\ml-agents
 cmd /k D:\Programs\Python\Virtual\MLAgents\Scripts\activate
---
+++ Training/Scripts/train_copy.bat
@@ -0,0 +1,17 @@
+@echo off
+
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+set "datestamp=%YY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
+set "fullstamp=%YY%%MM%%DD%_%HH%%Min%%Sec%"
+
+set UNITY_PROJECT_DIR_PATH=D:\Documents\_git\2DAI
+set TRAINING_DIR_PATH=%UNITY_PROJECT_DIR_PATH%\Training
+set BRAINS_DIR_PATH=%UNITY_PROJECT_DIR_PATH%\Assets\Brains
+set RUN_DIR_NAME=Run%fullstamp%
+
+mlagents-learn %TRAINING_DIR_PATH%\trainer_config.yaml --run-id=%RUN_DIR_NAME% --train
+for /F %%b in ('dir /b models\%RUN_DIR_NAME%\*.nn') do copy models\Run%fullstamp%\%%b %BRAINS_DIR_PATH%\%%~nb_%fullstamp%%%~xb
+
+if %ERRORLEVEL% EQU 0 exit
