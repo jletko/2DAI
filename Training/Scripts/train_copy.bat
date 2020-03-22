65358315188b7dc5c25ce4e4c91536d74d79e8d8
Juraj Letko <jletko@gmail.com>
2020-03-22 14:17:45 +0100

    Copied brains names date compacted.

--- Training/Scripts/train_copy.bat
+++ Training/Scripts/train_copy.bat
@@ -1,8 +1,8 @@
 for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
-set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "Y=%dt:~3,1%" & set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
 set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
 set "datestamp=%YY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
-set "fullstamp=%YY%%MM%%DD%_%HH%%Min%%Sec%"
+set "fullstamp=%Y%%MM%%DD%_%HH%%Min%%Sec%"
 
 set UNITY_PROJECT_DIR_PATH=%PROJECTS_DIRECTORY_PATH%\2DAI
 set TRAINING_DIR_PATH=%PROJECTS_DIRECTORY_PATH%\2DAI\Training
@@ -16,6 +16,6 @@ if [%1]==[] (
 )
 
 if not exist %BRAINS_DIR_PATH% MkDir %BRAINS_DIR_PATH%
-for /F %%b in ('dir /b models\%RUN_DIR_NAME%\*.nn') do copy models\Run%fullstamp%\%%b %BRAINS_DIR_PATH%\%%~nb_%fullstamp%%%~xb
+for /F %%b in ('dir /b models\%RUN_DIR_NAME%\*.nn') do copy models\Run%fullstamp%\%%b %BRAINS_DIR_PATH%\%%~nb%fullstamp%%%~xb
 
 if %ERRORLEVEL% EQU 0 exit
