f1cfc7f69ed4de877171e86c686b07adeb093a76
Juraj Letko <jletko@gmail.com>
2020-03-28 16:56:35 +0100

    Soft-Actor Critic batch option and configuration file added

--- Training/Scripts/train.bat
+++ Training/Scripts/train.bat
@@ -1,1 +1,11 @@
-start /min train_copy.bat %1
+@echo off
+set "TEMP_VAR=0"
+if [%1]==[p] set TEMP_VAR=1
+if [%1]==[s] set TEMP_VAR=1
+
+if %TEMP_VAR% equ 1 (
+	start /min train_copy.bat %*
+) else (
+	echo on
+	echo Missing p or s parameter
+)
