c6ac1b68784a7af42de50ed66eaf470e3a8f5774
Juraj Letko <jletko@gmail.com>
2023-04-29 09:29:25 +0200

    Update of board.bat script.

--- Training/Scripts/board.bat
+++ Training/Scripts/board.bat
@@ -1,4 +1,4 @@
 @echo off
 
-TASKLIST | FINDSTR "tensorboard" || start /min tensorboard --logdir=%PROJECTS_DIRECTORY_PATH%\%PROJECT_DIRECTORY%\Training\results --port 6006"
-start "Chrome" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --new-window "http://localhost:6006"
+TASKLIST | FINDSTR "tensorboard" || start /min tensorboard --logdir=%PROJECTS_DIRECTORY_PATH%\%PROJECT_DIRECTORY%\Training\scripts\results --port 6006"
+start "Chrome" "C:\Program Files\Google\Chrome\Application\chrome.exe" --new-window "http://localhost:6006"
